1ae9fd444d0326ae095f5de3a1d52dcea9a1e305
王一 WangYi <godspeed_china@yeah.net>
2019-03-06 22:09:05 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -20,20 +20,18 @@
    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
 */
-#ifndef __wyhash__
-#define __wyhash__
-#include	<stdint.h>
+#ifndef wyhash_included
+#define wyhash_included
 //the following functions should not be called outside the file
-inline	uint64_t	wyhashmix64(uint64_t	A,	uint64_t	B){	__uint128_t	r=A^0x60bee2bee120fc15ull;	r*=B^0xa3b195354a39b70dull;	return	(r>>64)^r;	}
-inline	unsigned	wyhashmix32(unsigned	A,	unsigned	B){	uint64_t	r=A^0x7b16763u;	r*=B^0xe4f5a905u;	return	(r>>32)^r;	}
-inline	uint64_t	wyhashread64(const	void	*const	ptr){	return	*(uint64_t*)(ptr);	}
-inline	uint64_t	wyhashread32(const	void	*const	ptr){	return	*(uint32_t*)(ptr);	}
-inline	uint64_t	wyhashread16(const	void	*const	ptr){	return	*(uint16_t*)(ptr);	}
-inline	uint64_t	wyhashread08(const	void	*const	ptr){	return	*(uint8_t*)(ptr);	}
-
+inline	unsigned long long	wyhashmix64(unsigned long long	A,	unsigned long long	B){	__uint128_t	r=A^0x60bee2bee120fc15ull;	r*=B^0xa3b195354a39b70dull;	return	(r>>64)^r;	}
+inline	unsigned	wyhashmix32(unsigned	A,	unsigned	B){	unsigned long long	r=A^0x7b16763u;	r*=B^0xe4f5a905u;	return	(r>>32)^r;	}
+inline	unsigned long long	wyhashread64(const	void	*const	ptr){	return	*(unsigned long long*)(ptr);	}
+inline	unsigned long long	wyhashread32(const	void	*const	ptr){	return	*(unsigned	int*)(ptr);	}
+inline	unsigned long long	wyhashread16(const	void	*const	ptr){	return	*(unsigned	short*)(ptr);	}
+inline	unsigned long long	wyhashread08(const	void	*const	ptr){	return	*(unsigned	char*)(ptr);	}
 //the following function is the general hash function to be called
-inline	uint64_t	wyhash(const void* key,	uint64_t	len, uint64_t	seed){
-	const	uint8_t	*ptr=(const	uint8_t*)key,	*const	end=ptr+len;
+inline	unsigned long long	wyhash(const void* key,	unsigned long long	len, unsigned long long	seed){
+	const	unsigned	char	*ptr=(const	unsigned	char*)key,	*const	end=ptr+len;
 	while(ptr+8<=end){	seed=wyhashmix64(seed,	wyhashread64(ptr));	ptr+=8;	}
 	switch(end-ptr){
 		case	1:	seed=wyhashmix64(seed,	wyhashread08(ptr));	break;
@@ -46,12 +44,9 @@ inline	uint64_t	wyhash(const void* key,	uint64_t	len, uint64_t	seed){
 	}
 	return	wyhashmix64(seed,	len);
 }
-
 //the following function is for 64bit integer hasing, the return value is consistant with the general funciton
-inline	uint64_t	wyhash64(uint64_t	key, uint64_t	seed){	return	wyhashmix64(wyhashmix64(seed,	key),	8);	}
-
+inline	unsigned long long	wyhash64(unsigned long long	key, unsigned long long	seed){	return	wyhashmix64(wyhashmix64(seed,	key),	8);	}
 //the following function is for 32bit integer hashing, the return value is NOT consistant with the general funciton
 inline	unsigned	wyhash32(unsigned	key, unsigned	seed){	return	wyhashmix32(wyhashmix32(seed,	key),	4);	}
-
 #endif
 

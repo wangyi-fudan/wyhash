0e5f7f57b0288a7d57fb649639c3c0fec0c6a90f
王一 WangYi <godspeed_china@yeah.net>
2019-03-07 07:17:55 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -27,12 +27,16 @@ inline	unsigned long long	wyhashmix64(unsigned long long	A,	unsigned long long	B
 #ifdef __SIZEOF_INT128__
 	__uint128_t	r=A;	r*=B^0xa3b195354a39b70dull;	return	(r>>64)^r;	
 #else
+	// compromised for compatibility
 	B^=0xa3b195354a39b70dull;
 	unsigned long long	ha=A>>32,	hb=B>>32,	la=(unsigned int)A,	lb=(unsigned int)B,	hi, lo;
 	unsigned long long	rh=ha*hb,	rm0=ha*lb,	rm1=hb*la,	rl=la*lb,	t=rl+(rm0<<32),	c=t<rl;
 	lo=t+(rm1<<32);	c+=lo<t;	hi=rh+(rm0>>32)+(rm1>>32)+c;
 	return hi^lo;
 #endif
+}
+inline	unsigned int	wyhashmix32(unsigned int	A,	unsigned int	B){
+	unsigned long long	r=(unsigned long long)A*(unsigned long long)B;	return	(r>>32)^r;	
 }
 inline	unsigned long long	wyhashread64(const	void	*const	ptr){	return	*(unsigned long long*)(ptr);	}
 inline	unsigned long long	wyhashread32(const	void	*const	ptr){	return	*(unsigned int*)(ptr);	}
@@ -52,5 +56,9 @@ inline	unsigned long long	wyhash(const void* key,	unsigned long long	len, unsign
 	case	6:	return	wyhashmix64(wyhashmix64(seed,(wyhashread32(ptr)<<16)|wyhashread16(ptr+4)),len);
 	case	7:	return	wyhashmix64(wyhashmix64(seed,(wyhashread32(ptr)<<24)|(wyhashread16(ptr+4)<<8)|wyhashread08(ptr+6)),len);
 	}
+}
+//the following function is specialied for very fast 32bit integer hashing.
+inline	unsigned int	wyhash32(unsigned int	key, unsigned int	seed){
+	return	wyhashmix32(wyhashmix32(seed^0x7b16763u,	key^0xe4f5a905u),	0x4a9e6939u);	
 }
 #endif

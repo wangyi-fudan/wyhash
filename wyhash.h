c2a131f97ce376a1940ae33368ac0c13b2e3f531
王一 WangYi <godspeed_china@yeah.net>
2019-03-22 00:39:16 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -7,7 +7,7 @@ static	inline	unsigned long long	_wymum(unsigned long long	A,	unsigned long long
 #ifdef __SIZEOF_INT128__
 	__uint128_t	r=A;	r*=B;	return	(r>>64)^r;
 #elif defined(_MSC_VER)
-	A=_umul128(A,	B,	&B);	return	A^B;
+	A=_umul128(A,	B,	&B);	return A^B;
 #else
 	unsigned long long	ha=A>>32,	hb=B>>32,	la=(unsigned int)A,	lb=(unsigned int)B,	hi, lo;
 	unsigned long long	rh=ha*hb,	rm0=ha*lb,	rm1=hb*la,	rl=la*lb,	t=rl+(rm0<<32),	c=t<rl;
@@ -20,7 +20,7 @@ static	inline	unsigned long long	_wyr08(const	unsigned char	*p){	unsigned char v
 static	inline	unsigned long long	_wyr16(const	unsigned char	*p){	unsigned short v;	memcpy(&v,	p,	2);	return	v;	}
 static	inline	unsigned long long	_wyr32(const	unsigned char	*p){	unsigned int v;	memcpy(&v,	p,	4);	return	v;	}
 static	inline	unsigned long long	_wyr64(const	unsigned char	*p){	unsigned long long v;	memcpy(&v,	p,	8);	return	v;	}
-#define __wyr64(p)	((_wyr32(p)<<32)|_wyr32(p+4))
+static	inline	unsigned long long	__wyr64(const	unsigned char	*p){	return	(_wyr32(p)<<32)|_wyr32(p+4);	}
 static	inline	unsigned long long	wyhash(const void* key,	unsigned long long	len, unsigned long long	seed){
 	const	unsigned char	*p=(const	unsigned char*)key;	unsigned long long i;
 	for(i=0;	i+32<=len;	i+=32,	p+=32)	seed=_wymum(seed^_wyp0,	_wymum(_wyr64(p)^_wyp1,_wyr64(p+8)^_wyp2)^_wymum(_wyr64(p+16)^_wyp3,_wyr64(p+24)^_wyp4));

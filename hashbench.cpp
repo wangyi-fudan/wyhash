9b8e23766e22c422c4b34d007a87a37eee400e9e
王一 WangYi <godspeed_china@yeah.net>
2019-11-05 10:46:38 +0800

    Update hashbench.cpp

--- hashbench.cpp
+++ hashbench.cpp
@@ -24,9 +24,9 @@ int	main(void){
 	dt=end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec);
 	cerr<<"wyhash_v3\t"<<1e-6*R*N/dt<<"\thashes/us\n";
 
-	for(size_t	i=0;	i<N;	i++)	dummy+=FNV1A_Pippip_Yurii(v[i].c_str(),	v[i].size(),0);
+	for(size_t	i=0;	i<N;	i++)	dummy+=FNV1A_Pippip_Yurii(v[i].c_str(),	v[i].size());
 	gettimeofday(&beg,NULL);
-	for(size_t  r=0;    r<R;    r++)	for(size_t	i=0;	i<N;	i++)	dummy+=FNV1A_Pippip_Yurii(v[i].c_str(), v[i].size(),r);
+	for(size_t  r=0;    r<R;    r++)	for(size_t	i=0;	i<N;	i++)	dummy+=FNV1A_Pippip_Yurii(v[i].c_str(), v[i].size());
 	gettimeofday(&end,NULL);
 	dt=end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec);
 	cerr<<"FNV1A_Pippip_Yurii\t"<<1e-6*R*N/dt<<"\thashes/us\n";

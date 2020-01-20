b8d33ad148344e7f0b41f5065acb289643ecde82
王一 WangYi <godspeed_china@yeah.net>
2020-01-20 22:13:05 +0800

    Add files via upload

--- wysum.c
+++ wysum.c
@@ -1,24 +1,23 @@
-#define WYHASH32
-#define	WYHANZI
+#define	WYHASH32
+#define	WYSUM
 #include	<sys/mman.h>
 #include	<sys/stat.h>
 #include	<stdio.h>
 #include	<unistd.h>
 #include	"wyhash.h"
 #include	<fcntl.h>
-void	wysum(const	char	*F){
+void	summary(const	char	*F){
 	int	fd;	struct	stat	sb;
 	fd=open(F,	O_RDONLY);
 	if(fd<0)	return;
 	fstat(fd,	&sb);
 	uint8_t	*p=(uint8_t*)mmap(NULL,	sb.st_size,	PROT_READ,	MAP_SHARED,	fd,	0);
 	if(p==MAP_FAILED)	return;
-	char	s[13];
-	wy2hanzi(s,	wyhash(p,sb.st_size,0));
+	char	s[15];
+	printf("%s\t%s\n",	wysum(p,sb.st_size,s),	F);
 	munmap(p,	sb.st_size);	close(fd);
-	printf("%s\t%s\n",	s,	F);
 }
 int	main(int	ac,	char	**av){
-	for(int	i=1;	i<ac;	i++)	wysum(av[i]);
+	for(int	i=1;	i<ac;	i++)	summary(av[i]);
 	return	0;
 }

2bd0a90ad8057959a2297a1c5afe75a9ea5d99fd
王一 WangYi <godspeed_china@yeah.net>
2020-01-20 21:19:39 +0800

    convert hash to chinese name
    I realize that hash function is the namer of virtual objection. Since I am Chinese and Chinese characters is rich alphabet (2048 here). We can name everything with a Chinese name. Just an optional macro WYHANZI, hope not to bothor  foreign friends :-)  wysum is similar to md5sum.

---
+++ wysum.c
@@ -0,0 +1,23 @@
+#define	WYHANZI
+#include	<sys/mman.h>
+#include	<sys/stat.h>
+#include	<stdio.h>
+#include	<unistd.h>
+#include	"wyhash.h"
+#include	<fcntl.h>
+void	wysum(const	char	*F){
+	int	fd;	struct	stat	sb;
+	fd=open(F,	O_RDONLY);
+	if(fd<0)	return;
+	fstat(fd,	&sb);
+	uint8_t	*p=(uint8_t*)mmap(NULL,	sb.st_size,	PROT_READ,	MAP_SHARED,	fd,	0);
+	if(p==MAP_FAILED)	return;
+	char	s[13];
+	wy2hanzi(s,	wyhash(p,sb.st_size,0));
+	munmap(p,	sb.st_size);	close(fd);
+	printf("%s\t%s\n",	s,	F);
+}
+int	main(int	ac,	char	**av){
+	for(int	i=1;	i<ac;	i++)	wysum(av[i]);
+	return	0;
+}

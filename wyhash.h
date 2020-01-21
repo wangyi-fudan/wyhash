1c79d6eb06adb1d789976e59a917832c371b29c6
王一 WangYi <godspeed_china@yeah.net>
2020-01-21 16:26:22 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -30,16 +30,21 @@ static	inline	uint64_t	_wyr4(const	uint8_t	*p) {	uint32_t v;	memcpy(&v,	p,	4);	r
 static	inline	uint64_t	_wyr3(const	uint8_t	*p,	unsigned	k) {	return	(((uint64_t)p[0])<<16)|(((uint64_t)p[k>>1])<<8)|p[k-1];	}
 static	inline	uint64_t	wyhash(const void* key,	uint64_t	len,	uint64_t	seed) {
 	const	uint8_t	*p=(const	uint8_t*)key;	uint64_t	i=len&63,	see1=seed;
-	if(!i) {	if(len==i)	goto	ret;	}
-	else	if(i<4){	seed=_wymum(_wyr3(p,i)^seed^_wyp0,seed^_wyp1);	if(len==i)	goto	ret;	}
-	else	if(i<=8){	seed=_wymum(_wyr4(p)^seed^_wyp0,_wyr4(p+i-4)^seed^_wyp1);	if(len==i)	goto	ret;	}
-	else	if(i<=16){	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+i-8)^seed^_wyp1);	if(len==i)	goto	ret;	}
-	else	if(i<=24) {	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);	see1=_wymum(_wyr8(p+i-8)^see1^_wyp2,see1^_wyp3);	if(len==i)	goto	ret;	} 
-	else	if(i<=32){	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);	see1=_wymum(_wyr8(p+16)^see1^_wyp2,_wyr8(p+i-8)^see1^_wyp3);	if(len==i)	goto	ret;	}
+	#if defined(__GNUC__) || defined(__INTEL_COMPILER)
+		#define	_like_equal_	__builtin_expect(len==i,1)
+	#else
+		#define _like_equal_  (len==i)
+	#endif
+	if(!i) {	if(_like_equal_) goto ret;	}
+	else	if(i<4){	seed=_wymum(_wyr3(p,i)^seed^_wyp0,seed^_wyp1);	if(_like_equal_) goto ret;	}
+	else	if(i<=8){	seed=_wymum(_wyr4(p)^seed^_wyp0,_wyr4(p+i-4)^seed^_wyp1);	if(_like_equal_) goto ret;	}
+	else	if(i<=16){	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+i-8)^seed^_wyp1);	if(_like_equal_) goto ret;	}
+	else	if(i<=24) {	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);	see1=_wymum(_wyr8(p+i-8)^see1^_wyp2,see1^_wyp3);	if(_like_equal_) goto ret;	} 
+	else	if(i<=32){	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);	see1=_wymum(_wyr8(p+16)^see1^_wyp2,_wyr8(p+i-8)^see1^_wyp3);	if(_like_equal_) goto ret;	}
 	else{	
 		seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1)^_wymum(_wyr8(p+16)^seed^_wyp2,_wyr8(p+24)^seed^_wyp3);	
 		see1=_wymum(_wyr8(p+i-32)^see1^_wyp1,_wyr8(p+i-24)^see1^_wyp2)^_wymum(_wyr8(p+i-16)^see1^_wyp3,_wyr8(p+i-8)^see1^_wyp0);	
-		if(len==i)	goto	ret;
+		if(_like_equal_) goto ret;
 	}
 	for(p+=i,i=len-i;	i>=64; i-=64,p+=64) {	
 		seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1)^_wymum(_wyr8(p+16)^seed^_wyp2,_wyr8(p+24)^seed^_wyp3);	

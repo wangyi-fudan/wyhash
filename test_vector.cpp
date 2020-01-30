4d78b10509d04fe98515d51158f4a03b81c8807b
王一 WangYi <godspeed_china@yeah.net>
2020-01-30 23:10:30 +0800

    Add files via upload

---
+++ test_vector.cpp
@@ -0,0 +1,12 @@
+#include	"wyhash/wyhash.h"
+#include	<iostream>
+#include	<vector>
+using	namespace	std;
+
+int	main(void){
+	vector<string>	v={"","a","abc","message digest","abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789","12345678901234567890123456789012345678901234567890123456789012345678901234567890"};
+	for(size_t	i=0;	i<v.size();	i++){
+		cout<<"wyhash(\""<<v[i]<<"\","<<i<<")="<<hex<<wyhash(v[i].c_str(),v[i].size(),i)<<'\n';
+	}
+	return	0;
+}

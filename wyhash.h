0e4b475443d8a50e4d22ad0a9f54bd695974edcd
王一 WangYi <godspeed_china@yeah.net>
2020-02-03 09:21:22 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -46,7 +46,6 @@ static	inline	uint64_t	_wyr4(const	uint8_t	*p)	{	unsigned	v;	memcpy(&v,  p,  4);
 #endif
 static	inline	uint64_t	_wyr3(const	uint8_t	*p,	unsigned	k) {	return	(((uint64_t)p[0])<<16)|(((uint64_t)p[k>>1])<<8)|p[k-1];	}
 static	inline	uint64_t	wyhash(const void* key,	uint64_t	len,	uint64_t	seed) {
-	const	uint8_t	*p=(const	uint8_t*)key;	uint64_t	i=len&63;
 	#if defined(__GNUC__) || defined(__INTEL_COMPILER)
 		#define	_like_(x)	__builtin_expect(x,1)
 		#define	_unlike_(x)	__builtin_expect(x,0)
@@ -54,6 +53,15 @@ static	inline	uint64_t	wyhash(const void* key,	uint64_t	len,	uint64_t	seed) {
 		#define _like_(x)  (x)
 		#define _unlike_(x)  (x)
 	#endif
+	const	uint8_t	*p=(const	uint8_t*)key;	uint64_t	i=len;
+	if(_unlike_(len>=64)){
+		uint64_t	see1=seed,	see2=seed,	see3=seed;
+		for(; i>=64; i-=64,p+=64){
+			seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);		see1=_wymum(_wyr8(p+16)^see1^_wyp2,_wyr8(p+24)^see1^_wyp3);	
+			see2=_wymum(_wyr8(p+32)^see2^_wyp1,_wyr8(p+40)^see2^_wyp2);	see3=_wymum(_wyr8(p+48)^see3^_wyp3,_wyr8(p+56)^see3^_wyp0);	
+		}
+		seed^=see1^see2^see3;
+	}
 	if(_unlike_(!i)) {	}
 	else	if(_unlike_(i<4))	seed=_wymum(_wyr3(p,i)^seed^_wyp0,seed^_wyp1);
 	else	if(_like_(i<=8))	seed=_wymum(_wyr4(p)^seed^_wyp0,_wyr4(p+i-4)^seed^_wyp1);
@@ -61,13 +69,7 @@ static	inline	uint64_t	wyhash(const void* key,	uint64_t	len,	uint64_t	seed) {
 	else	if(_like_(i<=24))	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1)^_wymum(_wyr8(p+i-8)^seed^_wyp2,seed^_wyp3);
 	else	if(_like_(i<=32))	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1)^_wymum(_wyr8(p+16)^seed^_wyp2,_wyr8(p+i-8)^seed^_wyp3);
 	else	seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1)^_wymum(_wyr8(p+16)^seed^_wyp2,_wyr8(p+24)^seed^_wyp3)^_wymum(_wyr8(p+i-32)^seed^_wyp1,_wyr8(p+i-24)^seed^_wyp2)^_wymum(_wyr8(p+i-16)^seed^_wyp3,_wyr8(p+i-8)^seed^_wyp0);
-	if(_like_(i==len))	return	_wymum(seed,len^_wyp4);
-	uint64_t	see1=seed,	see2=seed,	see3=seed;
-	for(p+=i,i=len-i;	_like_(i>=64); i-=64,p+=64) {
-		seed=_wymum(_wyr8(p)^seed^_wyp0,_wyr8(p+8)^seed^_wyp1);		see1=_wymum(_wyr8(p+16)^see1^_wyp2,_wyr8(p+24)^see1^_wyp3);	
-		see2=_wymum(_wyr8(p+32)^see2^_wyp1,_wyr8(p+40)^see2^_wyp2);	see3=_wymum(_wyr8(p+48)^see3^_wyp3,_wyr8(p+56)^see3^_wyp0);	
-	}
-	return	_wymum(seed^see1^see2,see3^len^_wyp4);
+	return	_wymum(seed,len^_wyp4);
 }
 static	inline	uint64_t	wyhash64(uint64_t	A, uint64_t	B) {	return	_wymum(_wymum(A^_wyp0,	B^_wyp1),	_wyp2);	}
 static	inline	uint64_t	wyrand(uint64_t	*seed) {	*seed+=_wyp0;	return	_wymum(*seed^_wyp1,*seed);	}
@@ -79,7 +81,7 @@ template	<uint64_t	Bits,	typename	KeyT,	typename	HashT,	typename	EqT=std::equal_
 static	inline	size_t	key2pos(const	KeyT	&key,	std::vector<KeyT>	&keys,	std::vector<bool>	&used){
 	HashT	hasher;		EqT	equaler;	uint64_t	h=hasher(key);
 	for(uint64_t	j=1,r=h;	j;	j++,	r=wyhash64(h,j)){
-		for(size_t	j=0;	j<16;	j++){	size_t	p=_wyrotr(r,j<<2)&((1ull<<Bits)-1);	if(equaler(key,keys[p])||!used[p])	return	p;	}
+		for(size_t	j=0;	j<16;	j++){	size_t	p=_wyrotr(r,j<<2)>>(64-Bits);	if(equaler(key,keys[p])||!used[p])	return	p;	}
 	}
 	return	~0ull;
 }

a7e4c4fe2e53ed87d707f448cd8f75e01bb2ad06
王一 WangYi <godspeed_china@yeah.net>
2020-02-04 12:45:36 +0800

    crazy bulk speed, remove hash table etc

--- wyhash.h
+++ wyhash.h
@@ -53,27 +53,28 @@ static	inline	uint64_t	wyhash(const void* key,	uint64_t	len,	uint64_t	seed,	cons
 	#define _like_(x)  (x)
 	#define _unlike_(x)  (x)
 #endif
-	const	uint8_t	*p=(const	uint8_t*)key;	uint64_t	i=len;	seed^=secret[4];	len^=secret[5];
-	label:
+	const	uint8_t	*p=(const	uint8_t*)key;	uint64_t	i=len;	seed^=secret[4];
+	if(_unlike_(len)>=64){
+		uint64_t	see1=seed,	see2=seed,	see3=seed;
+		for(;	_like_(i>=64); i-=64,p+=64){
+			seed=_wymum(_wyr8(p)^secret[0],_wyr8(p+8)^seed);		see1=_wymum(_wyr8(p+16)^secret[1],_wyr8(p+24)^see1);
+			see2=_wymum(_wyr8(p+32)^secret[2],_wyr8(p+40)^see2);	see3=_wymum(_wyr8(p+48)^secret[3],_wyr8(p+56)^see3);
+		}
+		seed^=see1^see2^see3;
+	}
+	len^=secret[5];
 	if(_unlike_(i<4))	return	_wymum(_wymum((_like_(i)?_wyr3(p,i):0)^secret[0],seed),len);
 	else	if(_like_(i<=8))	return	_wymum(_wymum(_wyr4(p)^secret[0],_wyr4(p+i-4)^seed),len);
 	else	if(_like_(i<=16))	return	_wymum(_wymum(_wyr8(p)^secret[0],_wyr8(p+i-8)^seed),len);
 	else	if(_like_(i<=32))	return	_wymum(_wymum(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymum(_wyr8(p+i-16)^secret[1],_wyr8(p+i-8)^seed),len);
-	else	if(_like_(i<=64))	return	_wymum(_wymum(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymum(_wyr8(p+16)^secret[1],_wyr8(p+24)^seed)
-				^_wymum(_wyr8(p+i-32)^secret[2],_wyr8(p+i-24)^seed)^_wymum(_wyr8(p+i-16)^secret[3],_wyr8(p+i-8)^seed),len);
-	uint64_t	see1=seed,	see2=seed,	see3=seed;
-	for(;	i>=64; i-=64,p+=64){
-		seed=_wymum(_wyr8(p)^secret[0],_wyr8(p+8)^seed);		see1=_wymum(_wyr8(p+16)^secret[1],_wyr8(p+24)^see1);
-		see2=_wymum(_wyr8(p+32)^secret[2],_wyr8(p+40)^see2);	see3=_wymum(_wyr8(p+48)^secret[3],_wyr8(p+56)^see3);
-	}
-	seed^=see1^see2^see3;
-	goto	label;
+	else	return	_wymum(_wymum(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymum(_wyr8(p+16)^secret[1],_wyr8(p+24)^seed)^_wymum(_wyr8(p+i-32)^secret[2],_wyr8(p+i-24)^seed)^_wymum(_wyr8(p+i-16)^secret[3],_wyr8(p+i-8)^seed),len);
 }
 static	inline	void	make_secret(uint64_t	seed,	uint64_t	secret[6]){
+	uint8_t	c[]={15,23,27,29,30,39,43,45,46,51,53,54,57,58,60,71,75,77,78,83,85,86,89,90,92,99,101,102,105,106,108,113,114,116,120,135,139,141,142,147,149,150,153,154,156,163,165,166,169,170,172,177,178,180,184,195,197,198,201,202,204,209,210,212,216,225,226,228,232,240};
 	for(size_t	i=0;	i<6;	i++){
 		uint8_t	ok;
-		do{	ok=1;	secret[i]=wyrand(&seed);
-			if(__builtin_popcountll(secret[i])!=32){	ok=0;	continue;	}
+		do{	ok=1;	secret[i]=0;
+			for(size_t	j=0;	j<64;	j+=8)	secret[i]|=((uint64_t)c[wyrand(&seed)%sizeof(c)])<<j;
 			for(size_t	j=0;	j<i;	j++)	if(__builtin_popcountll(secret[i]^secret[j])!=32)	ok=0;
 		}while(!ok);
 	}
@@ -81,49 +82,4 @@ static	inline	void	make_secret(uint64_t	seed,	uint64_t	secret[6]){
 static	inline	uint64_t	wyhash64(uint64_t	A, uint64_t	B) {	return	_wymum(_wymum(A^0xa0761d6478bd642full,B^0xe7037ed1a0b428dbull),0x8ebc6af09c88c6e3ull);	}
 static	inline	double	wy2u01(uint64_t	r) {	const	double	_wynorm=1.0/(1ull<<52);	return	(r>>11)*_wynorm;	}
 static	inline	double	wy2gau(uint64_t	r) {	const	double	_wynorm=1.0/(1ull<<20);	return	((r&0x1fffff)+((r>>21)&0x1fffff)+((r>>42)&0x1fffff))*_wynorm-3.0;	}
-#ifdef __cplusplus
-#include	<vector>
-template	<uint64_t	Bits,	typename	KeyT,	typename	HashT,	typename	EqT>	//  the minimum fast hash table/set
-static	inline	size_t	key2pos(const	KeyT	&key,	std::vector<KeyT>	&keys,	std::vector<bool>	&used){
-	HashT	hasher;		EqT	equaler;	uint64_t	h=hasher(key);
-	for(uint64_t	j=1,r=h;	j;	j++,	r=wyhash64(h,j)){
-		for(size_t	j=0;	j<16;	j++){	size_t	p=_wyrotr(r,j<<2)>>(64-Bits);	if(equaler(key,keys[p])||!used[p])	return	p;	}
-	}
-	return	~0ull;
-}
-/*	hashmap/hashset example
-#include	<iostream>
-#include	"wyhash.h"
-using	namespace	std;
-struct	hasher{	size_t	operator()(const	string	&s)const{	return	wyhash(s.c_str(),s.size(),0);	}};
-int	main(void){
-	std::vector<string>	keys(1ull<<20);	std::vector<bool>	used(1ull<<20);	std::vector<unsigned>	values(1ull<<20);
-	string	s;	size_t	pos=0;
-	for(cin>>s;	!cin.eof();	cin>>s){
-		pos=key2pos<20,string,	hasher>(s,keys,used);
-		if(!used[pos]){	keys[pos]=s;	used[pos]=true;	values[pos]=0;	}
-		else	values[pos]++;
-	}
-	return	pos;
-}*/
-//  the minimum bloom filter. paramters calculator: https://hur.st/bloomfilter/
-static	inline	void	bfpush(uint64_t	hash_of_key,	std::vector<bool>	&bitset,	size_t	size,	size_t	round){
-	for(size_t	i=0;	i<round;	i++)	bitset[(((__uint128_t)wyhash64(hash_of_key,i))*size)>>64]=true;
-}
-static	inline	size_t	bftest(uint64_t	hash_of_key,	std::vector<bool>	&bitset,	size_t	size,	size_t	round){
-	for(size_t	i=0;	i<round;	i++)	if(!bitset[(((__uint128_t)wyhash64(hash_of_key,i))*size)>>64])	return	false;
-	return	true;
-}
-/*bloom filter example
-#include	<iostream>
-#include	"wyhash.h"
-using	namespace	std;
-int	main(void){
-	size_t	size=0x2000000;	std::vector<bool>	bits(size);
-	string	s;
-	for(cin>>s;	!cin.eof();	cin>>s)		bfpush(wyhash(s.c_str(),s.size(),0),bits,size,4);
-	cout<<bftest(wyhash("abc",3,0),bits,size,4)<<'\n';
-	cout<<bftest(wyhash("abcdshk",7,0),bits,size,4)<<'\n';
-}*/
-#endif
 #endif

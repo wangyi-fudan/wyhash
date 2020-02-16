84b43eb7ac784951b797fec3444116dfb5a07d20
王一 WangYi <godspeed_china@yeah.net>
2020-02-16 09:56:44 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -1,18 +1,20 @@
 // Author: Wang Yi <godspeed_china@yeah.net>
-#ifndef wyhash_version_5
-#define wyhash_version_5
+#ifndef wyhash_version_6
+#define wyhash_version_6
 #include <stdint.h>
 #include <string.h>
 #if defined(_MSC_VER) && defined(_M_X64)
 #include <intrin.h>
 #pragma intrinsic(_umul128)
+#elif defined(__GNUC__)
+#include <x86intrin.h>
 #endif
 #if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
 #define _likely_(x) __builtin_expect(x,1)
 #else
 #define _likely_(x) (x)
 #endif
-const uint64_t _wyp[6]={0xa0761d6478bd642full,0xe7037ed1a0b428dbull,0x8ebc6af09c88c6e3ull,0x589965cc75374cc3ull,0x1d8e4e27c47d124full,0x72b22b96e169b471ull};//default secret
+const uint64_t _wyp[10]={0xb10f1ea5b4358d87ull,0x2e63952eb46a7127ull,0xd88be12db28d1769ull,0xb8d2a6b2d21e6c55ull,0xe43ad88ed1c9ac1bull,0xe4a33a5336a35a1dull,0xe85aca652dd266a5ull,0x2e1d72638e6c2e4bull,0x8ba6d4726c8db48dull,0x935a749a3ae2478bull};
 static inline uint64_t _wyrotr(uint64_t v, unsigned k){ return (v>>k)|(v<<(64-k)); }
 static inline uint64_t _wymum(uint64_t A, uint64_t B){
 #ifdef UNOFFICIAL_WYHASH_32BIT	//	fast on 32 bit system
@@ -30,13 +32,9 @@ static inline uint64_t _wymum(uint64_t A, uint64_t B){
 #endif
 #endif
 }
-static inline uint64_t _wymix(uint64_t A, uint64_t B){	
-#ifdef UNOFFICIAL_WYHASH_FAST //lose entropy with probability 2^-66 per byte
- return	_wymum(A,B);	
-#else
- return	A^B^_wymum(A,B);	
-#endif
-}
+static inline uint64_t _wymix(uint64_t A, uint64_t B){ return _wymum(A,B)^(A^B); }
+static inline uint64_t _wymix32(uint64_t A){ return A^((A>>32)*(uint32_t)A); }
+static inline uint64_t wyhash64(uint64_t A, uint64_t B){ return _wymum(_wymum(A^_wyp[0],B^_wyp[1]),_wyp[2]); }
 static inline uint64_t wyrand(uint64_t *seed){ *seed+=_wyp[0]; return _wymum(*seed^_wyp[1],*seed); }
 static inline double wy2u01(uint64_t r){ const double _wynorm=1.0/(1ull<<52); return (r>>11)*_wynorm; }
 static inline double wy2gau(uint64_t r){ const double _wynorm=1.0/(1ull<<20); return ((r&0x1fffff)+((r>>21)&0x1fffff)+((r>>42)&0x1fffff))*_wynorm-3.0; }
@@ -64,33 +62,68 @@ static inline uint64_t FastestHash(const void *key, size_t len, uint64_t seed){
  const uint8_t *p=(const uint8_t*)key;
  return _likely_(len>=4)?(_wyr4(p)+_wyr4(p+len-4))*(_wyr4(p+(len>>1)-2)^seed):(_likely_(len)?_wyr3(p,len)*(_wyp[0]^seed):seed);
 }
-static inline uint64_t _wyhash(const void* key, uint64_t len, uint64_t seed, const uint64_t secret[6]){
- const uint8_t *p=(const uint8_t*)key; uint64_t i=len; seed^=secret[4];
- if(_likely_(i<=64)){
+static inline uint64_t _wyhash(const void* key, uint64_t len, uint64_t seed, const uint64_t secret[10]){
+ const uint8_t *p=(const uint8_t*)key; uint64_t i=len; seed^=secret[8];
+ if(_likely_(i<=128)){
   label:
   if(_likely_(i>=8)){
    if(_likely_(i<=16)) return _wymix(_wyr8(p)^secret[0],_wyr8(p+i-8)^seed);
    else if(_likely_(i<=32)) return _wymix(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymix(_wyr8(p+i-16)^secret[1],_wyr8(p+i-8)^seed);
-   else return _wymix(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymix(_wyr8(p+16)^secret[1],_wyr8(p+24)^seed)
-    ^_wymix(_wyr8(p+i-32)^secret[2],_wyr8(p+i-24)^seed)^_wymix(_wyr8(p+i-16)^secret[3],_wyr8(p+i-8)^seed);
+   else{ seed=_wymix(_wyr8(p)^secret[0],_wyr8(p+8)^seed)^_wymix(_wyr8(p+16)^secret[1],_wyr8(p+24)^seed); i-=32; p+=32; goto label; }
   }
   else {
    if(_likely_(i>=4)) return _wymix(_wyr4(p)^secret[0],_wyr4(p+i-4)^seed);
    else return _wymix((_likely_(i)?_wyr3(p,i):0)^secret[0],seed);
   }
  }
- uint64_t see1=seed, see2=seed, see3=seed;
- for(; i>64; i-=64,p+=64){
-  seed=_wymix(_wyr8(p)^secret[0],_wyr8(p+8)^seed);  see1=_wymix(_wyr8(p+16)^secret[1],_wyr8(p+24)^see1);
-  see2=_wymix(_wyr8(p+32)^secret[2],_wyr8(p+40)^see2); see3=_wymix(_wyr8(p+48)^secret[3],_wyr8(p+56)^see3);
+#if defined(__AVX2__)
+ __m256i se[4]={ {seed,seed,seed,seed}, {seed,seed,seed,seed}, {seed,seed,seed,seed}, {seed,seed,seed,seed}},
+  ma[4]={
+  {secret[0]^secret[4],secret[0]^secret[5],secret[0]^secret[6],secret[0]^secret[7]},
+  {secret[1]^secret[4],secret[1]^secret[5],secret[1]^secret[6],secret[1]^secret[7]},
+  {secret[2]^secret[4],secret[2]^secret[5],secret[2]^secret[6],secret[2]^secret[7]},
+  {secret[3]^secret[4],secret[3]^secret[5],secret[3]^secret[6],secret[3]^secret[7]}};
+ for(; i>128; i-=128,p+=128){
+  __m256i d[4]={_mm256_loadu_si256((__m256i*)p),_mm256_loadu_si256((__m256i*)(p+32)),_mm256_loadu_si256((__m256i*)(p+64)),_mm256_loadu_si256((__m256i*)(p+96))};
+  for(size_t j=0; j<4; j++){
+   se[j]=_mm256_xor_si256(_mm256_xor_si256(ma[j],d[j]),se[j]);
+   se[j]=_mm256_xor_si256(se[j],_mm256_mul_epu32 (se[j],_mm256_shuffle_epi32(se[j],0x31)));
+  }
  }
- seed^=see1^see2^see3;
+ for(size_t	j=0; j<4; j++){ uint64_t *s=(uint64_t*)&se[j]; seed^=s[0]^s[1]^s[2]^s[3];}
+#elif defined(__SSE2__)
+ __m128i se[8]={ {seed,seed},{seed,seed}, {seed,seed},{seed,seed}, {seed,seed},{seed,seed}, {seed,seed},{seed,seed}},
+  ma[8]={
+  {secret[0]^secret[4],secret[0]^secret[5]},{secret[0]^secret[6],secret[0]^secret[7]},
+  {secret[1]^secret[4],secret[1]^secret[5]},{secret[1]^secret[6],secret[1]^secret[7]},
+  {secret[2]^secret[4],secret[2]^secret[5]},{secret[2]^secret[6],secret[2]^secret[7]},
+  {secret[3]^secret[4],secret[3]^secret[5]},{secret[3]^secret[6],secret[3]^secret[7]}};
+ for(; i>128; i-=128,p+=128){
+  __m128i d[8]={_mm_loadu_si128((__m128i*)p),_mm_loadu_si128((__m128i*)(p+16)),_mm_loadu_si128((__m128i*)(p+32)),_mm_loadu_si128((__m128i*)(p+48)),
+  _mm_loadu_si128((__m128i*)(p+64)),_mm_loadu_si128((__m128i*)(p+80)),_mm_loadu_si128((__m128i*)(p+96)),_mm_loadu_si128((__m128i*)(p+112))};
+  for(size_t j=0; j<8; j++){
+   se[j]=_mm_xor_si128(_mm_xor_si128(ma[j],d[j]),se[j]);
+   se[j]=_mm_xor_si128(se[j],_mm_mul_epu32 (se[j],_mm_shuffle_epi32(se[j],0x31)));
+  }
+ }
+ for(size_t	j=0; j<8; j++){ uint64_t *s=(uint64_t*)&se[j]; seed^=s[0]^s[1];}
+#else
+ uint64_t se[4][4]={ {seed,seed,seed,seed}, {seed,seed,seed,seed}, {seed,seed,seed,seed}, {seed,seed,seed,seed}};
+ uint64_t ma[4][4]={
+  {secret[0]^secret[4],secret[0]^secret[5],secret[0]^secret[6],secret[0]^secret[7]},
+  {secret[1]^secret[4],secret[1]^secret[5],secret[1]^secret[6],secret[1]^secret[7]},
+  {secret[2]^secret[4],secret[2]^secret[5],secret[2]^secret[6],secret[2]^secret[7]},
+  {secret[3]^secret[4],secret[3]^secret[5],secret[3]^secret[6],secret[3]^secret[7]}};
+ for(; i>128; i-=128,p+=128) for(size_t j=0; j<4; j++) for(size_t k=0; k<4; k++) se[j][k]=_wymix32(_wyr8(p+(j*4+k)*8)^se[j][k]^ma[j][k]);
+ for(size_t	j=0; j<4; j++) for(size_t k=0; k<4; k++) seed^=se[j][k];
+#endif
  goto label;
 }
-static inline uint64_t wyhash(const void* key, uint64_t len, uint64_t seed, const uint64_t secret[6]){ return _wymum(_wyhash(key,len,seed,secret)^len,secret[5]); }
-static inline void make_secret(uint64_t seed, uint64_t secret[6]){
+static inline uint64_t wyhash(const void* key, uint64_t len, uint64_t seed, const uint64_t secret[10]){  uint64_t h=_wyhash(key,len,seed,secret)^len; return _wymum(h,h^secret[9]);}
+
+static inline void make_secret(uint64_t seed, uint64_t secret[10]){
  uint8_t c[]= {15,23,27,29,30,39,43,45,46,51,53,54,57,58,60,71,75,77,78,83,85,86,89,90,92,99,101,102,105,106,108,113,114,116,120,135,139,141,142,147,149,150,153,154,156,163,165,166,169,170,172,177,178,180,184,195,197,198,201,202,204,209,210,212,216,225,226,228,232,240};
- for(size_t i=0; i<6; i++){
+ for(size_t i=0; i<10; i++){
   uint8_t ok;
   do{
    ok=1; secret[i]=0;
@@ -105,49 +138,5 @@ static inline void make_secret(uint64_t seed, uint64_t secret[6]){
 	for(size_t	j=2;	j<0x100000000ull;	j++)	if(secret[i]%j==0){	ok=0;	break;	}
   }while(!ok);
  }
-}
-static inline uint64_t wyhash64(uint64_t A, uint64_t B){ return _wymum(_wymum(A^_wyp[0],B^_wyp[1]),_wyp[2]); }
-typedef struct wyhash_context {
- uint64_t secret[5];
- uint64_t seed, see1, see2, see3;
- uint8_t buffer[64];
- uint8_t left; // always in [0, 64]
- int loop;
- uint64_t total;
-} wyhash_context_t;
-static inline void wyhash_init(wyhash_context_t *const __restrict ctx, const uint64_t seed, const uint64_t secret[5]){
- memcpy(ctx->secret, secret, sizeof(ctx->secret));
- ctx->seed=seed^secret[4]; ctx->see1=ctx->seed; ctx->see2=ctx->seed; ctx->see3=ctx->seed;
- ctx->left=0; ctx->total=0; ctx->loop=0;
-}
-static inline uint64_t _wyhash_loop(wyhash_context_t *const __restrict ctx, const uint8_t *p, const uint64_t len){
- uint64_t i = len; ctx->loop|=(i>64);
- for(; i>64; i-=64,p+=64){
-  ctx->seed=_wymix(_wyr8(p)^ctx->secret[0],_wyr8(p+8)^ctx->seed);
-  ctx->see1=_wymix(_wyr8(p+16)^ctx->secret[1],_wyr8(p+24)^ctx->see1);
-  ctx->see2=_wymix(_wyr8(p+32)^ctx->secret[2],_wyr8(p+40)^ctx->see2);
-  ctx->see3=_wymix(_wyr8(p+48)^ctx->secret[3],_wyr8(p+56)^ctx->see3);
- }
- return len - i;
-}
-static inline void wyhash_update(wyhash_context_t *const __restrict ctx, const void* const key, uint64_t len){
- ctx->total += len; // overflow for total length is ok
- const uint8_t* p = (const uint8_t*)key;
- uint8_t slots = 64 - ctx->left; // assert left <= 64
- slots = len <= slots ? len : slots;
- memcpy(ctx->buffer + ctx->left, p, slots);
- p += slots;
- len -= slots;
- ctx->left += slots;
- ctx->left -= _wyhash_loop(ctx, ctx->buffer, ctx->left + (len > 0));
- const uint64_t consumed = _wyhash_loop(ctx, p, len);
- p += consumed;
- len -= consumed; // assert len <= 64
- ctx->left = ctx->left > len ? ctx->left : (uint8_t)len;
- memcpy(ctx->buffer, p, len);
-}
-static inline uint64_t wyhash_final(wyhash_context_t *const __restrict ctx){
-  if(_likely_(ctx->loop)) ctx->seed ^= ctx->see1 ^ ctx->see2 ^ ctx->see3;
-  return _wymum(_wyhash(ctx->buffer, ctx->left, ctx->seed ^ ctx->secret[4], ctx->secret)^ctx->total,ctx->secret[4]);
 }
 #endif

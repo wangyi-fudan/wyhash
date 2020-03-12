7083c975d5a3ed56a36e8a0d694305e0212cfd39
王一 WangYi <godspeed_china@yeah.net>
2020-03-12 18:12:50 +0800

    Add files via upload

--- o1hash.h
+++ o1hash.h
@@ -12,8 +12,11 @@ static inline uint64_t o1hash(const void *key, size_t len) {
     memcpy(&first,p,4);
     memcpy(&middle,p+(len>>1)-2,4);
     memcpy(&last,p+len-4,4);
-    return  (uint64_t)(first+last)*middle;
+    return (uint64_t)(first+last)*middle;
   }
-  if(len) return ((((unsigned)p[0])<<16) | (((unsigned)p[len>>1])<<8) | p[len-1])*0xa0761d6478bd642full;
+  if(len){
+    uint64_t tail=((((unsigned)p[0])<<16) | (((unsigned)p[len>>1])<<8) | p[len-1]);
+    return tail*tail*tail;
+  }
   return  0;
 }

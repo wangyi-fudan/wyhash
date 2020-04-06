507278aafe125c1555354cceb88dba931a34ec85
王一 WangYi <godspeed_china@yeah.net>
2020-04-06 12:30:20 +0800

    Add files via upload

--- wyhash.h
+++ wyhash.h
@@ -3,13 +3,16 @@
 #define wyhash_version_gamma
 #include <stdint.h>
 #include <string.h>
-const	uint64_t	_wyp0=0xa0761d6478bd642full,	_wyp1=0xe7037ed1a0b428dbull;
+const	uint64_t	_wyp0=0xb10f1ea5b4358d87ull,	_wyp1=0x2e63952eb46a7127ull;
 static inline __uint128_t _wyread128(const void *p){ __uint128_t x; memcpy(&x,p,16); return x; }
 static inline __uint128_t _wymix128(__uint128_t x){ return (x>>64)*(uint64_t)x; }
 static inline uint64_t wyhash(const void *key, uint64_t len, __uint128_t seed){
- const __uint128_t _wyp=(((__uint128_t)_wyp0)<<64)|_wyp1; const uint8_t *p=(const uint8_t *)key; uint64_t len0=len; start:
- if(len<=16){ seed=_wymix128(_wymix128((_wyread128(p)<<((16-len)<<3))^seed^_wyp)^len0); return (seed>>64)^(uint64_t)seed; }
- else{ seed=_wymix128(_wyread128(p)^seed^_wyp); len-=16; p+=16; goto start; }
+ const __uint128_t _wyp=(((__uint128_t)_wyp0)<<64)|_wyp1;  
+ const uint8_t *p=(const uint8_t *)key;
+ uint64_t len0=len; 
+ for(;len>16;len-=16,p+=16) seed=_wymix128(_wyread128(p)^seed^_wyp);
+ seed=_wymix128(_wymix128((_wyread128(p)<<((16-len)<<3))^seed^_wyp)^len0); 
+ return (seed>>64)^(uint64_t)seed; 
 }
 static inline uint64_t wyhash64(uint64_t A, uint64_t B){ __uint128_t r=A^_wyp0; r*=B^_wyp1; r=(r>>64)*(uint64_t)r; return (r>>64)^(uint64_t)r;}
 static inline uint64_t wyrand(uint64_t *seed){ __uint128_t r=(*seed+=_wyp0); r*=*seed^_wyp1; return (r>>64)^(uint64_t)r;}

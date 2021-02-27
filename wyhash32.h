9ad38d04c826d56813557548ae9a401ccc473426
王一 WangYi <godspeed_china@yeah.net>
2021-02-27 08:27:21 +0800

    minor update

--- wyhash32.h
+++ wyhash32.h
@@ -18,7 +18,7 @@ static inline unsigned wyhash32(const void *key, uint64_t len, unsigned seed) {
   const uint8_t *p=(const uint8_t *)key; uint64_t i=len;
   unsigned see1=(unsigned)len; seed^=(unsigned)(len>>32); _wymix32(&seed, &see1);
   for(;i>8;i-=8,p+=8){  seed^=_wyr32(p); see1^=_wyr32(p+4); _wymix32(&seed, &see1); }
-  if(i>=4){ seed^=_wyr32(p); see1^=_wyr32(p+i-4); } else if (i) seed^=_wyr24(p,i);
+  if(i>=4){ seed^=_wyr32(p); see1^=_wyr32(p+i-4); } else if (i) seed^=_wyr24(p,(unsigned)i);
   _wymix32(&seed, &see1); _wymix32(&seed, &see1); return seed^see1;
 }
 static inline uint64_t wyrand(uint64_t *seed){  

c64f9b651795b7b4188491f22e2dba2d4841e935
王一 WangYi <godspeed_china@yeah.net>
2021-03-19 00:01:40 +0800

    Add files via upload

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,49 +1,28 @@
+#define	XXH_VECTOR 0
 #include	<sys/time.h>
 #include	<iostream>
 #include	<fstream>
-#include	<cstdlib>
-#include	<stdint.h>
-#include	<cstring>
 #include	<vector>
-#ifndef	XXH3
+#include	"xxHash/xxh3.h"
+#include	"t1ha/t1ha.h"
 #include	"wyhash.h"
-#else
-#include	"../xxHash/xxh3.h"
-#endif
 using	namespace	std;
-struct	ha{	
-  size_t	operator()(const	string	&s,	uint64_t seed)const{
-    #ifndef	XXH3
-    return  wyhash(s.c_str(),s.size(),seed,_wyp);	
-    #else
-    return  XXH3_64bits_withSeed(s.c_str(),s.size(),seed);	
-    #endif
-  }
-};
+struct	wy{	size_t	operator()(const	string	&s,	uint64_t seed)const{	return	wyhash(s.c_str(),s.size(),seed,_wyp);	}};
+struct	x3{	size_t	operator()(const	string	&s, uint64_t seed)const{	return	XXH3_64bits_withSeed(s.c_str(),s.size(),seed);	}};
+struct	xx{	size_t	operator()(const	string	&s, uint64_t seed)const{	return	XXH64(s.c_str(),s.size(),seed);	}};
+struct	t1{	size_t	operator()(const	string	&s, uint64_t seed)const{	return	t1ha2_atonce(s.c_str(),s.size(),seed);	}};
 
 vector<string>	v;	
 template <class Hasher>
 uint64_t	bench_hash(const	char	*name){
 	Hasher	h;	string	s;	timeval	beg,	end;	
-	uint64_t	dummy=0;	const	uint64_t	N=v.size(),	R=0x1000;
+	uint64_t	dummy=0;	const	uint64_t	N=v.size(),	R=0x10000;
 	cerr.precision(2);	cerr.setf(ios::fixed);	cerr<<'|'<<name<<(strlen(name)<8?"\t\t|":"\t|");
 	
 	gettimeofday(&beg,NULL);
 	for(size_t  r=0;    r<R;    r++)	for(size_t	i=0;	i<N;	i++)	dummy+=h(v[i],r);
 	gettimeofday(&end,NULL);
-	cerr<<1e-6*R*N/(end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec))<<"\t\t|";
-
-	s.resize(1<<8);
-	gettimeofday(&beg,NULL);
-	for(size_t  r=0;    r<(R<<14);    r++)	dummy+=h(s,r);
-	gettimeofday(&end,NULL);
-	cerr<<1e-9*(R<<14)*s.size()/(end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec))<<"\t\t|";
-
-	s.resize(1<<16);
-	gettimeofday(&beg,NULL);
-	for(size_t  r=0;    r<(R<<6);    r++)	dummy+=h(s,r);
-	gettimeofday(&end,NULL);
-	cerr<<1e-9*(R<<6)*s.size()/(end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec))<<"\t\t|\n";
+	cerr<<1e-6*R*N/(end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec))<<"\t\t|\n";
 	return	dummy;
 }
 
@@ -56,12 +35,11 @@ int	main(void){
 	for(size_t	i=v.size()-1;	i;	i--)	swap(v[i],v[rand()%(i+1)]);
 	uint64_t	r=0;
 	cerr<<file<<'\n';
-	cerr<<"|hash function\t|short hash/us\t|bulk_256B GB/s\t|bulk_64KB GB/s\t|\n";
-	cerr<<"|----\t\t|----\t\t|----\t\t|----\t\t|\n";
-    #ifndef	XXH3
-	r+=bench_hash<ha>("wyhash");
-    #else
-	r+=bench_hash<ha>("xxh3");
-    #endif
+	cerr<<"|hash function\t|short hash/us\t|\n";
+	cerr<<"|----\t\t|----\t\t|\n";
+	r+=bench_hash<wy>("wyhash");
+	r+=bench_hash<x3>("xxh3");
+	r+=bench_hash<xx>("xxHash64");
+	r+=bench_hash<t1>("t1ha2_atonce");
 	return	r;
 }

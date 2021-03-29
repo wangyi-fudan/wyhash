61c4c8c2f5feb7d8f40d8ded0c68828ab96bd494
王一 WangYi <godspeed_china@yeah.net>
2021-03-29 09:19:27 +0800

    Add files via upload

--- wyhashmap.cpp
+++ wyhashmap.cpp
@@ -1,63 +1,25 @@
-#include  <vector>
-#include  "wyhash.h"
+#include	<vector>
+#include	"wyhash.h"
+using	namespace	std;
 
-namespace  wy{
-template <typename KeyType, typename ValueType, typename Hasher, typename Equal=std::equal_to<KeyType>  >
-class  hashmap{
-private:
-  uint64_t  mask;
-  void  double_size(void){
-    mask=(capa*2)-1;
-    std::vector<std::pair<KeyType,ValueType>  >  data1(mask+1);
-    std::vector<uint8_t>  flag1(mask+1);
-    Hasher  hasher;
-    for(size_t  o=0;  o<capa;  o++)  if(flag[o]){
-      uint64_t  h0=hasher(data[o].first), h=h0, i;  uint8_t  f=h>>56;
-      for(uint64_t  i=0;  !f;  i++){  h=wyhash64(h0,i);  f=h>>56;  }
-      for(i=h&mask;flag1[i];i=(i+1)&mask);
-      flag1[i]=f;  data1[i]=std::move(data[o]);
-    }
-    capa*=2;  swap(data,data1);  swap(flag,flag1);
-  }
-public:
-  std::vector<std::pair<KeyType,ValueType>  >  data;
-  std::vector<uint8_t>  flag;	// non-zero indicates used; zero indicates empty
-  uint64_t  capa, size;
-
-  hashmap(size_t  log2capacity=4){  
-    capa=1ull<<log2capacity; if(capa<16) capa=16;  mask=capa-1;  
-    data.resize(capa);  flag.resize(capa);
-    size=0;
-  }
-
-  void  clear(void){
-    std::vector<uint8_t>(capa).swap(flag);
-    std::vector<std::pair<KeyType,ValueType> >(capa).swap(flag);
-    size=0;
-  }
-
-  ValueType&  operator[](KeyType  &key){
-    Hasher  hasher; Equal equal; 
-    uint64_t  h0=hasher(key), h=h0, i;  uint8_t  f=h>>56;
-    for(uint64_t  i=0;  !f;  i++){  h=wyhash64(h0,i);  f=h>>56;  }
-    for(i=h&mask;flag[i]&&(flag[i]!=f||!equal(data[i].first,key));i=(i+1)&mask);
-    if(flag[i])  return  data[i].second;
-    if(size<0.9*capa){  flag[i]=f;  data[i].first=key;  size++;  return  data[i].second;  }
-    double_size();
-    for(i=h&mask;flag[i];i=(i+1)&mask);
-    flag[i]=f;  data[i].first=key;  size++;  return  data[i].second;
-  }
-
-  // return capa if not found
-  uint64_t find(KeyType &key){
-    Hasher  hasher; Equal equal; 
-    uint64_t  h0=hasher(key), h=h0, i;  uint8_t  f=h>>56;
-    for(uint64_t  i=0;  !f;  i++){  h=wyhash64(h0,i);  f=h>>56;  }
-    for(i=h&mask;flag[i]&&(flag[i]!=f||!equal(data[i].first,key));i=(i+1)&mask);
-    return flag[i]?i:capa;
-  }
-
-  void erase(uint64_t pos){ if(pos<capa&&flag[pos]){ flag[pos]=0; std::pair<KeyType,ValueType>().swap(data[pos]); size--; }  }
-
+template <typename KeyType, typename ValueType, typename Hasher, size_t	log2capa>
+struct	wyhashmap{
+	const	size_t	mask=(1ull<<log2capa)-1;
+	vector<std::pair<uint64_t,ValueType>	>	data;
+	wyhashmap(){	data.resize(1ull<<log2capa);	}
+	void	clear(void){	std::vector<std::pair<uint64_t,ValueType> >(1ull<<log2capa).swap(data);	}
+	ValueType&	operator[](KeyType	&key){
+		Hasher	hasher;
+		size_t	i=1; uint64_t	s;	do{	s=hasher(key,i);	i++;	}while(_unlikely_(!s));
+		for(i=hasher(key,0)&mask;data[i].first&&data[i].first!=s;i=(i+1)&mask);
+		if(!data[i].first)	data[i].first=s;	
+		return	data[i].second;
+	}
+	size_t	find(KeyType  &key){
+		Hasher	hasher;
+		size_t	i=1; uint64_t	s;	do{	s=hasher(key,i);	i++;	}while(_unlikely_(!s));
+		for(i=hasher(key,0)&mask;data[i].first&&data[i].first!=s;i=(i+1)&mask);
+		if(!data[i].first)	return	1ull<<log2capa;
+		return	i;
+	}
 };
-}
